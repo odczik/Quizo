e00c787d2f9c669b631a6ca7fbf8d0978cb5caf9
odczik <ondrej.dostal007@gmail.com>
2026-05-06 19:06:01 +0200

    Add browse tab to navbar

--- frontend/app/components/layouts/Navbar.tsx
+++ frontend/app/components/layouts/Navbar.tsx
@@ -8,6 +8,7 @@ export default function Navbar() {
                 <Link to="/" className="text-2xl font-bold text-black hover:none">Quizo</Link>
                 <ul className="flex gap-6 items-center">
                     <li><Link to="/demo" variant="subtle">Components Library Demo</Link></li>
+                    <li><Link to="/browse" className="hover:underline">Browse Quizzes</Link></li>
                     <li><Button variant="secondary" onClick={() => location.href = "/login"}>Login</Button></li>
                     <li><Button variant="primary" onClick={() => location.href = "/game"}>Join Game</Button></li>
                 </ul>
