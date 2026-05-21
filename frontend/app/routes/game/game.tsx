50714db22b5e70c16905f2b5e3a103a707ca9a0f
odczik <ondrej.dostal007@gmail.com>
2026-05-21 08:41:03 +0200

    Update game.tsx

--- frontend/app/routes/game/game.tsx
+++ frontend/app/routes/game/game.tsx
@@ -126,7 +126,6 @@ export default function Game() {
                             {question.question_index} / {question.questions_length}
                         </div>
                     )}
-                    {/* {!isHost ? ( */}
                     {answered ? (
                         results ? (
                             <div className="flex-1 flex flex-col items-center justify-center w-full h-full">
