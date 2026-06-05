460f762ce41b721130418831225f03387cd9151b
odczik <ondrej.dostal007@gmail.com>
2026-06-05 16:14:32 +0200

    Update game-container.tsx

--- frontend/app/routes/game/game-container.tsx
+++ frontend/app/routes/game/game-container.tsx
@@ -14,17 +14,15 @@ export default function GameContainer({ params }: { params: { id?: string } }) {
 
     useEffect(() => {
         if (lastMessage) {
-            console.log("Received message:", `'${lastMessage}'`);
-            console.log("Received message type:", `'${lastMessage.type}'`);
-            console.log("Is it game_start?", lastMessage.type === "game_start");
-            if (lastMessage.type === "game_start") {
-                console.log("Setting game state to in-game");
-                setGameState("in-game");
-            } else if (lastMessage.type === 'player_left') {
-                console.log('Player left:', lastMessage.username);
-                notify(`${lastMessage.username} has left the game.`, "warning");
+            switch (lastMessage.type) {
+                case "game_start":
+                    setGameState("in-game");
+                    break;
+                case "player_left":
+                    notify(`${lastMessage.username} has left the game.`, "warning");
+                    break;
             }
-        }
+        }        
     }, [lastMessage]);
 
     switch (gameState) {
